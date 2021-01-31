9ba7a3b3a72c9c040f8fdd74f8631c4e4e98ebee
WOJCIECH LEHMANN <lehmann.wojciech@icloud.com>
2021-01-31 17:55:10 +0100

    removed comments

--- src/components/VGallery/VGallery.js
+++ src/components/VGallery/VGallery.js
@@ -1,182 +1,22 @@
 import axios from 'axios';
 
-window.axios = require('axios');
-
 export default {
         name: 'VGallery',
     data() {
         return {
-            // posts: {
-            //     posts: null,
-            // },
+ 
             options: {
                 shareEl: false,
                 showHideOpacity: true,
                 getThumbsBoundsFn: false
             },
              items: [
-                // {
-                //     src: 'https://picsum.photos/id/1/600/400',
-                //     thumbnail: 'https://picsum.photos/id/1/340/340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/600x400',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 600,
-                //     h: 400,
-                //     alt: 'some numbers on a grey background' // optional alt attribute for thumbnail image
-                // },
-                // {
-                //     src: 'http://via.placeholder.com/1200x900',
-                //     thumbnail: 'http://via.placeholder.com/340x340',
-                //     w: 1200,
-                //     h: 900
-                // }
+
             ]
         };
     },
     
     created() {
-        // here you will do the api call for the images
         const API = 'http://iwona.cms.everse.pl/wp-json/wp/v2/media?parent=5&per_page=50'
         let items = []
         
@@ -202,37 +42,7 @@ export default {
 
     },
 
-    mounted() {
-        
-    },
-
     components: {
         VPictureSwipe: require("vue-picture-swipe").default
     },
 }
-
-
-
-
-// let items = []
-// const API = 'http://iwona.cms.everse.pl/wp-json/wp/v2/media?parent=5&per_page=50'
-
-// axios.get(API)
-// .then(res => 
-//     items.push({
-//     src: res.data[0].media_details.sizes.full.source_url,
-//     thumbnail: res.data[0].media_details.sizes.thumbnail.source_url,
-//     w: res.data[0].media_details.sizes.full.width,
-//     h: res.data[0].media_details.sizes.full.height,
-//     alt: res.data[0].alt_text
-// }))
-
-// this.items = items;
-
-// items.push({
-//         src: res.data[0].media_details.sizes.full.source_url,
-//         thumbnail: res.data[0].media_details.sizes.thumbnail.source_url,
-//         w: res.data[0].media_details.sizes.full.width,
-//         h: res.data[0].media_details.sizes.full.height,
-//         alt: res.data[0].alt_text
-//     }))
